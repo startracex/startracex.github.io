f25998bba4d9cc65a136dbdac8cde8e63bf2741b
Shiro Wang <startraceebr@foxmail.com>
2025-08-09 14:14:11 +0800

    update content styles

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -50,25 +50,32 @@ export default function ArticleCard({ title, description, date }: ArticleCardPro
 
   return (
     <div
-      className={cn(
-        "flex flex-col justify-between h-full w-full relative overflow-hidden p-6 ease-in-out",
-        isHovered && (gradientData.textColor === "black" ? "text-zinc-900" : "text-zinc-100"),
-      )}
+      className={cn("flex flex-col justify-between min-h-48 h-full relative overflow-hidden p-6 ease-in-out")}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="z-10 relative">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
-        <p className="text-sm mb-4">{description}</p>
+        <h3
+          className={cn(
+            "text-foreground text-xl font-semibold mb-2",
+            isHovered && (gradientData.textColor === "black" ? "text-zinc-900" : "text-zinc-100"),
+          )}
+        >
+          {title}
+        </h3>
+        <p
+          className={cn(
+            "text-foreground/80 text-sm mb-4",
+            isHovered && (gradientData.textColor === "black" ? "text-zinc-800" : "text-zinc-300"),
+          )}
+        >
+          {description}
+        </p>
       </div>
       <time
         className={cn(
-          "text-xs z-10 relative",
-          isHovered
-            ? gradientData.textColor === "black"
-              ? "text-zinc-900"
-              : "text-zinc-200"
-            : "text-zinc-600 dark:text-zinc-400",
+          "text-foreground/80 text-xs z-10 relative",
+          isHovered && (gradientData.textColor === "black" ? "text-zinc-900" : "text-zinc-200"),
         )}
       >
         {date}
